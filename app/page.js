953934da6488fd88566887c07aa75a7790c20a19
Eshika rawat <eshikarawat02@gmail.com>
2024-05-03 17:07:50 +0530

    added

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ const Homepage = () => {
   const route = useRouter();
   return (
     <header className="w-screen h-screen flex justify-center items-center cursor-pointer">
-      <motion.div initial={{ y: "-200%" }} animate={{ y: 0 }} transition={{ duration: 2, ease: "easeInOut" }} className="w-[900px] h-[500px] rounded-50 p-6 shadow-xl flex homecarddiv" onClick={() => route.push('/home')}>
+      <motion.div initial={{ y: "-200%" }} animate={{ y: 0 }} transition={{ duration: 2, ease: "easeInOut" }} className="w-[362px] h-[210px] md:w-[900px] md:h-[500px] md:rounded-50 p-6 shadow-xl flex homecarddiv" onClick={() => route.push('/home')}>
       </motion.div>
     </header>
   )
