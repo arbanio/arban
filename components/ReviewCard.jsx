89a0c03117715997262f3793995fbf1610e97b03
Eshika rawat <eshikarawat02@gmail.com>
2024-05-13 10:35:49 +0530

    added

--- components/ReviewCard.jsx
+++ components/ReviewCard.jsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const ReviewCard = ({ name, key, description, img}) => {
+const ReviewCard = ({ name, description, img}) => {
   return (
-    <div key={key} className="w-[250px] xl:w-[350px] xl:h-[370px] border-2 border-gold-1 rounded-5 flex flex-col gap-3">
+    <div className="w-[120px] sm:w-[180px] md:w-[250px] xl:w-[350px] xl:h-[370px] border-2 border-gold-1 rounded-5 flex flex-col gap-1 sm:gap-3">
         <div className="flex justify-center mt-5 w-full h-[100px]"><Image src={img} width={100} height={100} alt="profile" className="rounded-[150px]"  /></div>
-        <h2 className="text-base xl:text-xl font-semibold uppercase text-center" style={{ fontFamily: 'Amarante'}}>{name}</h2>
+        <h2 className="text-xs sm:text-base xl:text-xl font-semibold uppercase text-center" style={{ fontFamily: 'Amarante'}}>{name}</h2>
         <div className="flex justify-center">
             <Image src={"/assets/rating.svg"} alt="rating" width={100} height={100} />
         </div>
-        <p className="mt-5 text-center text-xs xl:text-sm" style={{ fontFamily: "Open Sans"}}>{description}</p>
-        <Link className="text-gold-1 text-center underline text-xs xl:text-sm mb-5 xl:mb-0 xl:mt-5" href={"/"} style={{ fontFamily: "Open Sans"}}>Learn more</Link>
+        <p className="mt-5 text-center w-24 m-auto sm:w-full text-[10px] sm:text-xs xl:text-sm" style={{ fontFamily: "Open Sans"}}>{description}</p>
+        <Link className="text-gold-1 text-center underline text-[10px] sm:text-xs xl:text-sm mb-5 xl:mb-2 xl:mt-5" href={"/"} style={{ fontFamily: "Open Sans"}}>Learn more</Link>
     </div>
   )
 }
--- pages/Reviews.jsx
+++ pages/Reviews.jsx
@@ -1,41 +1,73 @@
-import ReviewCard from "@/components/ReviewCard";
+'use client'
 
+import ReviewCard from "@/components/ReviewCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 
 const Reviews = () => {
-  
-    const reviewData = [
+  const reviewData = [
     {
-        name: "Eshika Rawat",
-        img: "/assets/review3.jpeg",
-        description: "I like how this scent blends warmth and sweetness without the sweetness becoming too much or the almond dominating."
+      name: "Eshika Rawat",
+      img: "/assets/review3.jpeg",
+      description:
+        "I like how this scent blends warmth and sweetness without the sweetness becoming too much or the almond dominating.",
     },
 
     {
-        name: "Mukesh Sahu",
-        img: "/assets/review2.jpg",
-        description: "I like how this scent blends warmth and sweetness without the sweetness becoming too much or the almond dominating."
+      name: "Mukesh Sahu",
+      img: "/assets/review2.jpg",
+      description:
+        "I like how this scent blends warmth and sweetness without the sweetness becoming too much or the almond dominating.",
+    },
+
+    {
+      name: "Priyanshu",
+      img: "/assets/review1.jpg",
+      description:
+        "I like how this scent blends warmth and sweetness without the sweetness becoming too much or the almond dominating.",
     },
 
     {
         name: "Priyanshu",
         img: "/assets/review1.jpg",
-        description: "I like how this scent blends warmth and sweetness without the sweetness becoming too much or the almond dominating."
-    }
+        description:
+          "I like how this scent blends warmth and sweetness without the sweetness becoming too much or the almond dominating.",
+    },
   ];
 
   return (
     <main className="w-full h-[600px] md:h-screen bg-transparent relative z-40">
-        <div className="w-full flex flex-col justify-center">
-            <h1 className="text-xl md:text-4xl font-semibold mt-20 xl:mt-40 flex justify-center" style={{ fontFamily: 'Amarante'}}>REVIEWS</h1>
+      <div className="w-full flex flex-col justify-center">
+        <h1
+          className="text-xl md:text-4xl font-semibold mt-20 xl:mt-40 flex justify-center"
+          style={{ fontFamily: "Amarante" }}
+        >
+          REVIEWS
+        </h1>
 
-            <div className="mt-10 xl:mt-20 flex justify-around">
-                {reviewData.map((data) => (
-                    <ReviewCard name={data.name} img={data.img} key={data.name} description={data.description} />
-                ))}
-            </div>
+        <div className="mt-10 md:mt-12 xl:mt-20 flex sm:w-[90vw] sm:m-auto h-[400px] sm:h-[450px]">
+          <Swiper
+            slidesPerView={3}
+            pagination={{ clickable: true }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            {reviewData.map((data) => (
+            <SwiperSlide className='m-1 sm:m-1 md:m-1 xl:m-4' key={data.name}>
+              <ReviewCard
+                name={data.name}
+                img={data.img}
+                description={data.description}
+              />
+            </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
 export default Reviews;
