ebb8a525535ae32fa57e5061aae731f0084dc900
eshika rawat <eshikarawat02@gmail.com>
2024-06-03 01:24:10 +0530

    added homepage

--- app/page.js
+++ app/page.js
@@ -1,18 +1,45 @@
-"use client"
+"use client";
 
-import { motion }from 'framer-motion';
-import { useRouter } from 'next/navigation';
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const Homepage = () => {
+  const [isVisible, setIsVisible] = useState(true);
   const route = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+      setTimeout(() => {
+        route.push('/home');
+      }, 1000);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [route]);
+
   return (
     <header className="w-screen h-screen flex justify-center items-center cursor-pointer">
-      <motion.div initial={{ y: "-200%" }} animate={{ y: 0 }} transition={{ duration: 2, ease: "easeInOut" }} className="w-[362px] h-[210px] md:w-[900px] md:h-[500px] md:rounded-50 p-6 shadow-xl flex" onClick={() => route.push('/home')}>
-        <div className=' w-2/5 p-3 flex justify-center overflow-hidden relative items-center box'></div>
-        <div className='w-3/5 p-3 flex items-center justify-center'><img src='/assets/Arbanhomelogo.svg' /></div>
+      <motion.div
+        initial={{ y: "-200%" }}
+        animate={{ y: 0 }}
+        transition={{ duration: 2, ease: "easeInOut" }}
+        className="flex justify-center p-6 "
+        onClick={() => route.push("/home")}
+      >
+        <motion.div 
+        initial={{ opacity: 1 }}
+        animate={{ opacity: isVisible ? 1 : 0 }}
+        transition={{ duration: 1 }}
+        className="justify-center overflow-hidden relative items-center">
+          <Image src="/assets/Seahome.png" alt="sea-horse" width={260} height={260} />
+        </motion.div>
+        {/* <div className='w-3/5 p-3 flex items-center justify-center'><img src='/assets/Arbanhomelogo.svg' /></div> */}
       </motion.div>
     </header>
-  )
-}
+  );
+};
 
 export default Homepage;
